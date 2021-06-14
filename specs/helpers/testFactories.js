992bf8bdffe8f2701618a547d50d22b3cac3cd5f
SaepudinJeh <saepudinjeh@gmail.com>
2021-06-14 20:05:59 +0700

    Add files via upload

---
+++ specs/helpers/testFactories.js
@@ -0,0 +1,11 @@
+import LikeButtonInitiator from '../../src/scripts/utils/like-button-initiator';
+
+const createLikeButtonPresenterWithMovie = async (movie) => {
+	await LikeButtonInitiator.init({
+		likeButtonContainer: document.querySelector('#likeButtonContainer'),
+		movie
+	});
+};
+
+
+export { createLikeButtonPresenterWithMovie };
